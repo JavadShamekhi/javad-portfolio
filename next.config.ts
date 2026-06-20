67074e709067c0dfe34ccd9bde4361ef4fe71544
JavadShamekhi <javad.shamekhi.80@gmail.com>
2026-06-20 21:10:36 +0330

    change next version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	allowedDevOrigins: ['172.23.192.1'],
+	devIndicators: false
+
 };
 
 export default nextConfig;
